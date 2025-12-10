25eb542c426a5c66aafde8718e09a67123accd50
Matthew Johnston <matthewjohnston89@sky.com>
2025-12-10 19:24:41 +0000

    Complete 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(a) ||
+    a < 0 ||
+    b < 0
+  ) {
+    return "ERROR";
+  }
 
+  if (b > a) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else {
+    for (let j = b; j <= a; j++) {
+      sum += j;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
