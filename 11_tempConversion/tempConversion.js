9632d881391bd7f2c6c059741ad0fe31e4728cde
Matthew Johnston <matthewjohnston89@sky.com>
2025-12-10 19:55:05 +0000

    Complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  let fToC = ((farenheit - 32) * 5) / 9;
+  return Math.round(fToC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  let cToF = (celcius * 9) / 5 + 32;
+  return Math.round(cToF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
